f8a30c78ee0919d146f000b64c963a55a686d8f9
Andrew Ding <andrewxding@yahoo.com>
2018-04-25 13:25:51 -0700

    filtering with dropdown

--- src/components/ProfilesPage.js
+++ src/components/ProfilesPage.js
@@ -2,10 +2,99 @@ import React from "react";
 // import ProfileOverlay from "./ProfileOverlay.js";
 import "../sass/ProfilesPage.css";
 var data = {"data": {"data.aml": {"profiles": [{"position": "President", "candidates": [{"name": "Joe Bruin Pres", "image": "https://www.w3schools.com/images/w3schools_green.jpg", "slate": "Bruins United", "platform": ["this is one", "this is another", "and here is a third"]}, {"name": "Joe Bruin", "image": "https://www.w3schools.com/images/w3schools_green.jpg", "slate": "Bruins United", "platform": ["this is one", "this is another", "and here is a third"]}]}, {"position": "External VP President", "candidates": [{"name": "Joe Bruin Pres VP", "image": "https://www.w3schools.com/images/w3schools_green.jpg", "slate": "Bruins United", "platform": ["this is one", "this is another", "and here is a third"]}, {"name": "Joe Bruin VP", "image": "https://www.w3schools.com/images/w3schools_green.jpg", "slate": "Bruins United VP", "platform": ["this is one", "this is another", "and here is a third"]}]}], "sanctions": [{"link": "http://features.dailybruin.com/2018/coachella", "time": "04/23/18 2:32PM", "title": "Didn\u2019t put a sticker on laptop", "recipients": ["Joe Bruin"]}, {"link": "http://features.dailybruin.com/2018/coachella", "time": "04/21/18 2:32PM", "title": "Didn\u2019t put a sticker on laptop", "recipients": ["Joe Bruin"]}]}}};
-var candidates = []
-var images = [];
-const imgUrls = ['https://source.unsplash.com/PC_lbSSxCZE/800x600','https://source.unsplash.com/lVmR1YaBGG4/800x600','https://source.unsplash.com/5KvPQc1Uklk/800x600','https://source.unsplash.com/GtYFwFrFbMA/800x600'];
 
+
+class DropDown extends React.Component {
+  // static propTypes = {
+  //   className: React.PropTypes.string
+  // };
+  // static defaultProps = {
+  //   className: ''
+  // };
+  constructor(...args){
+    super(...args);
+
+    this.state = {
+      active: false
+    };
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    this.handleDocumentClick = this.handleDocumentClick.bind(this);
+    this.renderSelectionCont = this.renderSelectionCont.bind(this);
+  }
+  toggleDropDown(action, e){   
+    switch (action){
+      case 'close':
+        this.setState({
+          active: false
+        });
+        document.removeEventListener('click', this.handleDocumentClick); 
+        break;
+      default:
+        this.setState({
+          active: true
+        });
+        document.addEventListener('click', this.handleDocumentClick);
+        break
+    }
+  }
+  handleSelection(item){
+    this.props.onClick(item);
+    this.toggleDropDown('close');
+  }
+  handleDocumentClick(){
+    this.toggleDropDown('close');   
+  }
+  handleOutsideClick(e){
+    e.nativeEvent.stopImmediatePropagation();
+  }
+  renderSelectionCont(){  
+    if (!this.state.active) return;
+
+    return <DropDownItems options={this.props.options} onClick={this.handleSelection} displayField={this.props.displayField} />;
+  }
+
+  render(){
+    let wrapperClassName = 'bd-dropdown' + (this.props.className ? ' ' + this.props.className : '');
+    let caretClass = 'fa fa-chevron-down';
+    let toggle = 'open';
+    if (this.state.active) {
+      caretClass = 'fa fa-chevron-up';
+      wrapperClassName = wrapperClassName + ' __active';
+      toggle = 'close';
+    }
+
+    return (
+      <div className={wrapperClassName} onClick={this.handleOutsideClick}>
+        <div className='__control' onClick={this.toggleDropDown.bind(this, toggle)}>
+          <div>{this.props.value}</div>
+          <i className={caretClass}></i>
+        </div>
+        {this.renderSelectionCont()}
+      </div>
+    );
+  }
+}
+
+class DropDownItems extends React.Component {
+  constructor(...args){
+    super(...args);
+  }
+  render(){
+    let options = this.props.options.map((item, idx) => {
+      
+      return (
+        <li className='__item' key={idx} onClick={this.props.onClick.bind(this, item)}>{item.display}</li>
+      );
+    });
+    return (
+      <ul className='__options' onClick={this.props.onOutsideClick}>
+        {options}
+      </ul>
+    );
+  }
+}
 class ProfileOverlay extends React.Component {
   constructor() {
     super();
@@ -32,7 +121,7 @@ class ProfileOverlay extends React.Component {
       return null;
     }
     return (
-      <div>
+      <div id="mount">
         <div className="modal-overlay" onClick={closeModal}></div>
         <div isopen={(!!src).toString()} className="modal">
           <div className='modal-body'>
@@ -65,24 +154,36 @@ class ProfilePage extends React.Component {
     this.state = {
     	// candidates: JSON.parse(xmlHttp.responseText)['data']['data.aml']['profiles'],
     	candidates: data['data']['data.aml']['profiles'],
-    	currentIndex: null
+    	currentIndex: null,
+    	displayValue: 'All'
     }
     // this.ca = [];
-    this.state.candidates.forEach(function(position){
+    var dropdownOptions = [{display: 'All', value: 0}]
+    var candidates = [];
+    var images = [];
+    this.handleSelection = this.handleSelection.bind(this);
+    this.state.candidates.forEach(function(position, index){
+    	dropdownOptions.push({display: position.position, value: index + 1});
     	position.candidates.forEach(function(candidate){
     		candidate.position = position;
     		candidates.push(candidate);
     		images.push(candidate.image);
     	})
     })
+    this.images = images;
+    this.candidatesAll = candidates;
+    this.dropdownOptions = dropdownOptions
     this.closeModal = this.closeModal.bind(this);
     this.findNext = this.findNext.bind(this);
     this.findPrev = this.findPrev.bind(this);
     // this.renderImageContent = this.renderImageContent.bind(this);
   }
-
+  handleSelection(item){
+    this.setState({
+      displayValue: item.display
+    })
+  }
   openModal(e, index) {
-  	console.log('index', index, candidates[index].image);
     this.setState ({ currentIndex: index });
   }
   closeModal(e) {
@@ -111,7 +212,6 @@ class ProfilePage extends React.Component {
 		// TODO: onclick handler passes in whole candidate
   printCandidates(candidates, offset){
   	const candidateCards = candidates.map(function(candidate, index){
-  		console.log(candidate.image);
   		let style = {
   			backgroundImage: "url(" + candidate.image + ")",
   		};
@@ -125,18 +225,18 @@ class ProfilePage extends React.Component {
   }
   printPositions(){
   	let index = 0;
-  	console.log(candidates);
   	const positions = this.state.candidates.map(function(positionInfo){
   		index += positionInfo.candidates.length;
-  		return <div className="positionRow">
-  			<div className="positionName">
-  				{positionInfo.position}
-  			</div>
-  			<div className="positionCandidates">
-  				{this.printCandidates(positionInfo.candidates, index - positionInfo.candidates.length)}
-  			</div>
-  		</div>
-  	}, this);
+  		if (this.state.displayValue == positionInfo.position || this.state.displayValue == "All")
+	  		return <div className="positionRow">
+		  			<div className="positionName">
+		  				{positionInfo.position}
+		  			</div>
+		  			<div className="positionCandidates">
+		  				{this.printCandidates(positionInfo.candidates, index - positionInfo.candidates.length)}
+		  			</div>
+		  		</div>
+	  }, this);
 
   	return positions;
   }
@@ -144,15 +244,18 @@ class ProfilePage extends React.Component {
   render() {
     return(
       <div>
+      	<div className='dropdown-width'>
+        <DropDown options={this.dropdownOptions} value={this.state.displayValue} onClick={this.handleSelection} />
+      </div>
         {this.printPositions()}
         <ProfileOverlay
           closeModal={this.closeModal} 
           findPrev={this.findPrev} 
           findNext={this.findNext} 
           hasPrev={this.state.currentIndex > 0} 
-          hasNext={this.state.currentIndex + 1 < candidates.length} 
-          src={images[this.state.currentIndex]}
-          candidate={candidates[this.state.currentIndex]}
+          hasNext={this.state.currentIndex + 1 < this.candidatesAll.length} 
+          src={this.images[this.state.currentIndex]}
+          candidate={this.candidatesAll[this.state.currentIndex]}
         />
       </div>
     );
