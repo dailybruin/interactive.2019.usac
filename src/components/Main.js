9cf2f7325c4466766f24ac387d0e56c1bfe63fce
Alex Zhao <alexzhao21@gmail.com>
2018-04-25 18:15:14 -0700

    Set up referenda page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,16 +4,18 @@ import EndorsementsPage from "./EndorsementsPage";
 import ResultsPage from "./ResultsPage";
 import SanctionsPage from "./SanctionsPage";
 import ProfilePage from "./ProfilesPage";
+import ReferendaPage from "./ReferendaPage";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path='/candidates' component={ProfilePage}/>
-          <Route path='/endorsements' component={EndorsementsPage}/>
-          <Route path='/results' component={ResultsPage}/>
-          <Route path='/violations' component={SanctionsPage}/>
+          <Route exact path="/candidates" component={ProfilePage} />
+          <Route path="/endorsements" component={EndorsementsPage} />
+          <Route path="/results" component={ResultsPage} />
+          <Route path="/violations" component={SanctionsPage} />
+          <Route path="/referenda" component={ReferendaPage} />
         </Switch>
       </div>
     );
